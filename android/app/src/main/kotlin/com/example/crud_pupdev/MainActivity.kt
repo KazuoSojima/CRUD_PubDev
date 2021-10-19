05a9699f4d72bbb698e77cfe82e7b3773c19cfb6
KazuoSojima <kazuosojima@gmail.com>
2021-10-19 01:22:19 -0300

    appBar ok

---
+++ android/app/src/main/kotlin/com/example/crud_pupdev/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.crud_pupdev
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/home.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+class Home extends StatefulWidget {
+  const Home({Key? key}) : super(key: key);
+
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Lista de Carros'),
+        backgroundColor: Colors.grey,
+        actions: [
+          IconButton(
+            onPressed: () {},
+            icon: Icon(Icons.add),
+          ),
+        ],
+      ),
+    );
+  }
+}
